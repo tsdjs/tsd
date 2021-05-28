8fe3924909a71c21c3ff1caef540dc501bbd17b4
Dimitri B <BendingBender@users.noreply.github.com>
2021-05-28 15:35:22 +0200

    Allow `expectError` directives to detect parameter count mismatches (#102)

--- source/test/fixtures/expect-error/functions/index.test-d.ts
+++ source/test/fixtures/expect-error/functions/index.test-d.ts
@@ -1,8 +1,10 @@
 import {expectError} from '../../../..';
-import one, {three} from '.';
+import one, {two, three} from '.';
 
 expectError(one(true, true));
 expectError(one('foo', 'bar'));
+
+expectError(two('foo', 'bar'));
 
 // Produces multiple type checker errors in a single `expectError` assertion
 expectError(three(['a', 'bad']));
--- source/test/fixtures/expect-error/generics/index.test-d.ts
+++ source/test/fixtures/expect-error/generics/index.test-d.ts
@@ -1,6 +1,8 @@
 import {expectError} from '../../../..';
-import one from '.';
+import one, {two} from '.';
 
 expectError(one(true, true));
 
 expectError(one<number>(1, 2));
+
+expectError(two<number, string>(1, 'bar'));
