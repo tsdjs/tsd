1bf710b6842f81b4b20b6d15201edf15896e1e72
Dimitri B <semtexx@web.de>
2021-05-31 22:30:45 +0200

    Add tests for identicality checks of `any`/`never` type parameters to `expectType`/`expectNotType`
    Closes #78
    Closes #82

--- source/test/fixtures/identicality/identical/index.test-d.ts
+++ source/test/fixtures/identicality/identical/index.test-d.ts
@@ -6,3 +6,8 @@ expectType<number>(concat(1, 2));
 
 expectType<any>(concat(1, 2));
 expectType<string | number>(concat('unicorn', 'rainbow'));
+
+expectType<false>(concat(1, 2) as any);
+
+expectType<string>('' as never);
+expectType<any>('' as never);
