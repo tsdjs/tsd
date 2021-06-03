009a18521f8822e669ecee0f12003d9ccf4384d9
Dimitri B <BendingBender@users.noreply.github.com>
2021-06-03 12:09:53 +0200

    Add tests for identicality checks of `any`/`never` type parameters to `expectType`/`expectNotType` (#113)
    Closes #78
    Closes #82

--- source/test/fixtures/identicality/not-identical/index.test-d.ts
+++ source/test/fixtures/identicality/not-identical/index.test-d.ts
@@ -5,3 +5,6 @@ expectNotType<number>(concat('foo', 'bar'));
 expectNotType<string | number>(concat('foo', 'bar'));
 
 expectNotType<string>(concat('unicorn', 'rainbow'));
+
+expectNotType<false>(concat('foo', 'bar') as any);
+expectNotType<any>(concat('foo', 'bar') as any);
--- source/test/identicality.ts
+++ source/test/identicality.ts
@@ -8,7 +8,10 @@ test('identical', async t => {
 
 	verify(t, diagnostics, [
 		[7, 0, 'error', 'Parameter type `any` is not identical to argument type `number`.'],
-		[8, 0, 'error', 'Parameter type `string | number` is declared too wide for argument type `string`.']
+		[8, 0, 'error', 'Parameter type `string | number` is declared too wide for argument type `string`.'],
+		[10, 0, 'error', 'Parameter type `false` is not identical to argument type `any`.'],
+		[12, 0, 'error', 'Parameter type `string` is declared too wide for argument type `never`.'],
+		[13, 0, 'error', 'Parameter type `any` is declared too wide for argument type `never`.'],
 	]);
 });
 
@@ -16,6 +19,7 @@ test('not identical', async t => {
 	const diagnostics = await tsd({cwd: path.join(__dirname, 'fixtures/identicality/not-identical')});
 
 	verify(t, diagnostics, [
-		[7, 0, 'error', 'Parameter type `string` is identical to argument type `string`.']
+		[7, 0, 'error', 'Parameter type `string` is identical to argument type `string`.'],
+		[10, 0, 'error', 'Parameter type `any` is identical to argument type `any`.'],
 	]);
 });
