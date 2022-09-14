90ec6021706796a4bec436ff2be26100880095af
tommy-mitchell <tmitchell7@uh.edu>
2022-09-14 14:12:40 -0500

    Add tests (#142)

--- source/test/fixtures/expect-error/generics/index.test-d.ts
+++ source/test/fixtures/expect-error/generics/index.test-d.ts
@@ -1,8 +1,12 @@
-import {expectError} from '../../../..';
-import one, {two} from '.';
+import {expectError, expectType} from '../../../..';
+import one, {two, inferrable} from '.';
 
 expectError(one(true, true));
 
 expectError(one<number>(1, 2));
 
 expectError(two<number, string>(1, 'bar'));
+
+expectError(expectType<number>(inferrable<true>()));
+
+expectError(expectType<number>(inferrable()));
