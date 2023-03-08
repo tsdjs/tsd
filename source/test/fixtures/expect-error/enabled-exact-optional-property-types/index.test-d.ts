9b3cb0333412c69b8673bb790586e29d338d3212
tommy-mitchell <tmitchell7@uh.edu>
2023-03-07 18:04:44 -0600

    chore: add documentation

--- source/test/fixtures/expect-error/enabled-exact-optional-property-types/index.test-d.ts
+++ source/test/fixtures/expect-error/enabled-exact-optional-property-types/index.test-d.ts
@@ -4,18 +4,18 @@ import {OptionalProperty, setWithOptionalProperty} from '.';
 expectError(() => {
 	const obj: OptionalProperty = {
 		requiredProp: 'required',
-		// ts2375
+		// ts2375 - setting optional property to undefined
 		optionalProp: undefined,
 	};
 });
 
 expectError(setWithOptionalProperty({
 	requiredProp: 'required',
-	// ts2379
+	// ts2379 - setting optional property to undefined in a parameter
 	optionalProp: undefined,
 }));
 
 const obj: OptionalProperty = { requiredProp: 'required' };
 
-// ts2412
+// ts2412 - setting optional property to undefined by access
 expectError(obj.optionalProp = undefined);
