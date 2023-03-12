5f5f2784bcc16352385f546ac5208ae55f739cac
Sébastien Mischler <skarab@bluewin.ch>
2023-03-12 10:43:04 +0100

    chore: setup vitest

---
+++ test/life.test.ts
@@ -0,0 +1,5 @@
+import {expect, test} from 'vitest';
+
+test('life', () => {
+	expect(42).toBe(42)
+});
---
+++ vitest.config.ts
@@ -0,0 +1,8 @@
+import {defineConfig} from 'vite'
+
+export default defineConfig({
+  test: {
+    include: ['test/**/*.test.ts'],
+    exclude: ['test/fixtures/**/*']
+  },
+})
